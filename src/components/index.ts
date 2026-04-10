7080e0d90f5ee458cb6204794323e46562815fe8
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2026-04-09 21:47:45 -0300

    Add files via upload

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Exportação centralizada dos componentes
+ * 
+ * Permite importar assim:
+ * import { ConsultaCard } from './src/components';
+ * 
+ * Em vez de:
+ * import ConsultaCard from './src/components/ConsultaCard';
+ */
+
+export { default as ConsultaCard } from "./ConsultaCard";
