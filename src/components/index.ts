4a9c4a8c803e87f51cd79829d7979092ddcdfb73
lucasdias0812 <lucasdias081205@gmail.com>
2026-04-30 22:28:49 -0300

    COMMIT 30/4 - LUCASDIAS

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,4 +8,10 @@
  * import ConsultaCard from './src/components/ConsultaCard';
  */
 
+/**
+ * Exportação centralizada dos componentes
+ */
+
 export { default as ConsultaCard } from "./ConsultaCard";
+// export { default as Loading } from "./Loading";
+// export { default as EmptyState } from "./EmptyState";
--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,11 +1,14 @@
 import { Medico } from "./medico";
 import { Paciente } from "../types/paciente";
 import { StatusConsulta } from "../types/statusConsulta";
+import { Especialidade } from "../types/especialidade";
 export interface Consulta {
   id: number;
   medico: Medico;
   paciente: Paciente;
-  data: Date;
+  especialidade: Especialidade;
+  usuarioId: number;
+  data: string;
   valor: number;
   status: StatusConsulta;
   observacoes?: string;
